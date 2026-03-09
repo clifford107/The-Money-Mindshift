c03b96cc9133015c1db2a0a0ba3769bbda26a767
clifford107 <83698947+clifford107@users.noreply.github.com>
2026-03-09 15:02:22 +0200

    Refactor Firebase authentication implementation

--- js/auth.js
+++ js/auth.js
@@ -1,35 +1,29 @@
 <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB46vgL9d6dOkPIg-HzfHN_lB1IX3RSvv8",
-    authDomain: "the-money-mind-shift-60f9a.firebaseapp.com",
-    projectId: "the-money-mind-shift-60f9a",
-    storageBucket: "the-money-mind-shift-60f9a.firebasestorage.app",
-    messagingSenderId: "756290997216",
-    appId: "1:756290997216:web:a66b86d07539c83273f985",
-    measurementId: "G-ZXWTGJ9WFW"
-  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-
-document.getElementById("loginForm")?.addEventListener("submit", e=>{
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyB46vgL9d6dOkPIg-HzfHN_lB1IX3RSvv8",
+  authDomain: "the-money-mind-shift-60f9a.firebaseapp.com",
+  projectId: "the-money-mind-shift-60f9a",
+  storageBucket: "the-money-mind-shift-60f9a.firebasestorage.app",
+  messagingSenderId: "756290997216",
+  appId: "1:756290997216:web:a66b86d07539c83273f985",
+  measurementId: "G-ZXWTGJ9WFW"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+document.getElementById("loginForm")?.addEventListener("submit", (e) => {
   e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
-  auth.signInWithEmailAndPassword(email,password)
-    .then(()=>window.location="dashboard.html")
-    .catch(err=>alert(err.message));
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
 });
+</script>
